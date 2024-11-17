2ca132a2e43409c711d681dc1bf1f59bb1917bd6
DevanshPaliwal <78657285+DevanshPaliwal@users.noreply.github.com>
2024-11-17 18:13:54 +0530

    Add files via upload

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,11 +3,11 @@ import { ClerkProvider, useAuth } from '@clerk/clerk-expo';
 import { Ionicons } from '@expo/vector-icons';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { useFonts } from 'expo-font';
-import { Link, Stack, useRouter } from 'expo-router';
+import { Link, Stack, useRouter, useSegments } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
-import { TouchableOpacity } from 'react-native';
+import { TouchableOpacity,Text } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import 'react-native-reanimated';
 const CLERK_PUBLISHABLE_KEY=process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
@@ -52,6 +52,7 @@ const InitialLayout = () => {
   const router = useRouter()
 
   const {isLoaded,isSignedIn}=useAuth();
+  const segments=useSegments();
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
   useEffect(() => {
@@ -66,10 +67,21 @@ const InitialLayout = () => {
 
   useEffect(()=>{
     console.log('isSignedIn: ',isSignedIn)
+    if(!isLoaded) return;
+
+    const inAuthGroup = segments[0] === '(authenticated)';
+
+    if(isSignedIn && !inAuthGroup) {
+      router.replace('/(authenticated)/(tabs)/home');
+    }
+    else if(!isSignedIn){
+      router.replace('/');
+    }
+
   },[isSignedIn])
 
-  if (!loaded) {
-    return null;
+  if (!loaded || !isLoaded) {
+    return <Text>Loading...</Text>;
   }
 
   return (
@@ -134,6 +146,21 @@ const InitialLayout = () => {
             <Ionicons name="chevron-back-outline" size={30} color={Colors.dark} />
           </TouchableOpacity>
         )
+      }} />
+      <Stack.Screen name='(authenticated)/(tabs)' 
+      options={{
+        // title: 'Help',
+        // presentation: 'modal',
+        // headerTitleAlign: 'center',
+        // headerStyle: {
+        //   backgroundColor: '#e6e9fa',
+        // },
+        headerShown:false
+        // headerLeft: () => (
+        //   <TouchableOpacity onPress={router.back}>
+        //     <Ionicons name="chevron-back-outline" size={30} color={Colors.dark} />
+        //   </TouchableOpacity>
+        // ),
       }} />
     </Stack>
 
