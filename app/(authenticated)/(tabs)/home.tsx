44e5de7aaea63176231e90e910bf5f25963f9eb5
DevanshPaliwal <78657285+DevanshPaliwal@users.noreply.github.com>
2024-11-22 00:23:28 +0530

    Add files via upload

--- app/(authenticated)/(tabs)/home.tsx
+++ app/(authenticated)/(tabs)/home.tsx
@@ -1,12 +1,64 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Button } from 'react-native'
 import React from 'react'
+import { ScrollView } from 'react-native-gesture-handler';
+import Colors from '@/constants/Colors';
+import RoundBtn from '@/app/components/RoundBtn';
+import Dropdown from '@/app/components/Dropdown';
 
-const home = () => {
+const Page = () => {
+  const balance=1420; // current amount of balance
+
+  const onAddMoney=()=>{
+
+  }
+
   return (
-    <View>
-      <Text>home</Text>
-    </View>
+    <ScrollView style={{backgroundColor:Colors.background}}>
+      <View style={styles.account}>
+        <View style={styles.row}>
+        <Text style={styles.balance}>{balance}</Text>
+        <Text style={styles.currency}>$</Text>
+        </View>
+      </View>
+
+      <View style={styles.actionRow}>
+        <RoundBtn icon={'add'} text={'Add money'} onPress={onAddMoney} />
+        <RoundBtn icon={'refresh'} text={'Exchange'} />
+        <RoundBtn icon={'list'} text={'Details'}  />
+        <Dropdown  />
+      </View>
+    </ScrollView>
   )
 }
 
-export default home
+const styles=StyleSheet.create({
+  account:{
+    margin:80,
+    alignItems:'center',
+
+  },
+  row:{
+    flexDirection:'row',
+    alignItems:'center',
+    justifyContent:'center',
+    gap:10
+  },
+  balance:{
+    fontSize:50,
+    fontWeight:'bold',
+  },
+  currency:{
+    fontSize:20,
+    fontWeight:'500',
+    marginLeft:5,
+  },
+  actionRow:{
+    flexDirection:'row',
+    justifyContent:'space-between',
+    padding:20,
+  }
+})
+
+
+
+export default Page
